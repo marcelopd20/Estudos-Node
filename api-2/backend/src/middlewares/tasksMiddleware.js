4ecc3e7b35ed9ca300afb862c74076f7803feb30
marcelopd20 <marcelopd20@gmail.com>
2022-11-26 06:21:03 -0300

    api-2

---
+++ api-2/backend/src/middlewares/tasksMiddleware.js
@@ -0,0 +1,33 @@
+const validateFieldTitle = (req, res, next) => {
+    const { body } = req;
+
+    if (body.title === undefined) {
+        return res.status(400).json({ message: 'The field "title" is required' });
+    }
+
+    if (body.title === '') {
+        return res.status(400).json({ message: 'Title cant be empty' });
+    }
+
+    next();
+};
+
+const validateFieldStatus = (req, res, next) => {
+    const { body } = req;
+
+    if (body.status === undefined) {
+        return res.status(400).json({ message: 'The field "status" is required' });
+    }
+
+    if (body.status === '') {
+        return res.status(400).json({ message: 'status cant be empty' });
+    }
+
+    next();
+};
+
+module.exports = {
+    validateFieldTitle,
+    validateFieldStatus,
+
+};
