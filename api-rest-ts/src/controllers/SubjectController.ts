5285f71b22d7c691eb7d67a71e209a9a9806dc76
marcelopd20 <marcelopd20@gmail.com>
2022-11-28 10:33:42 -0300

    Estudo TS e Node

---
+++ api-rest-ts/src/controllers/SubjectController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { subjectRepository } from "../repositories/subjectRepository";
+
+export class SubjectController {
+    async create(req: Request, res: Response){
+        // create a subject
+        const { name } = req.body
+
+        if (!name) {
+            return res.status(400).json({ message: 'O nome é obrigatório' })
+        }
+
+        try {
+
+            const newSubject = subjectRepository.create({ name })
+
+            await subjectRepository.save(newSubject)
+
+            return res.status(201).json(newSubject)
+            
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json({ message: 'Internal Server Error' })
+        }
+    }
+}
---
+++ api-rest-ts/src/controllers/VideoController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { videoRepository } from "../repositories/videoRepository";
+
+export class videoController {
+
+}
